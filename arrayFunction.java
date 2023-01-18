66db536b07ff748ab7a2beafd7e1b05102c6f393
Rafis <Rafis@gmail.com>
2023-01-18 15:47:27 +0500

    edit

---
+++ arrayFunction.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class arrayFunction {
+    public static void main(String[] args) {
+        //4. Заполнить массив из десяти элементов значениями, вводимыми с клавиатуры в ходе
+        //выполнения программы.
+        int [] array = new int[10];
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите значения массива:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc1.nextInt();
+            System.out.print(array[i] + " ");
+        }
+        sc1.close();
+    }
+}
